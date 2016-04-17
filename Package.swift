ff0dc05698bf625d868e051a56a2a0e6d271e8a1
Damian Carrillo <dcarrillo@atlassian.com>
2016-04-16 23:22:21 -0500

    Removing integration test target

--- Package.swift
+++ Package.swift
@@ -3,9 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SwiftRedis",
     targets: [
-      Target(
-        name: "RedisIntegrationTests",
-        dependencies: [.Target(name: "hiredis")]),
       Target(name: "Redis"),
       Target(name: "hiredis")
     ],
